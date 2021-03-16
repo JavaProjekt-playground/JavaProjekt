991ac1e6af0bc3e36bf35d919428d448e6eeb296
Rok Žerdoner <rok.zerdoner@scv.si>
2021-03-16 13:28:08 +0100

    almost every add_function

--- Database/Functions/add_functions.sql
+++ Database/Functions/add_functions.sql
@@ -4,7 +4,7 @@ $$
 DECLARE
 	res regions%ROWTYPE;
 BEGIN
-	INSERT INTO regions (name, postcode) VALUES (nameR, postcodeR);
+	INSERT INTO regions (name, postcode) VALUES (nameR, postcodeR)
 	RETURNING * INTO res;
 	RETURN NEXT res;
 END
@@ -16,9 +16,67 @@ $$
 DECLARE
 	res reservation_statuses%ROWTYPE;
 BEGIN
-	INSERT INTO reservation_statuses (title, description) VALUES (titleR, descriptionR);
+	INSERT INTO reservation_statuses (title, description) VALUES (titleR, descriptionR)
 	RETURNING * INTO res;
 	RETURN NEXT res;
 END
 $$LANGUAGE 'plpgsql';
 
+CREATE OR REPLACE FUNCTION add_review (
+	messageR TEXT,
+	scoreR REVIEW_SCORE,
+	playfield_idR INTEGER,
+	user_idR INTEGER
+)
+RETURNS SETOF reviews AS
+$$
+DECLARE
+	res reviews%ROWTYPE;
+BEGIN
+	INSERT INTO reviews (message, score, playfield_id, user_id) VALUES (messageR, scoreR, playfield_idR, user_idR)
+	RETURNING * INTO res;
+	RETURN NEXT res;
+END
+$$LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION add_reservation (
+	from_dateR TIMESTAMP,
+	to_dateR TIMESTAMP,
+	user_idR INTEGER,
+	playfield_idR INTEGER,
+	paid BOOL
+)
+RETURNS SETOF reservations AS
+$$
+DECLARE
+	res reservations%ROWTYPE;
+BEGIN
+	INSERT INTO reservations (from_date, to_date, user_id, playfield_id, paid) VALUES (from_dateR, to_dateR, user_idR, playfield_idR, paidR)
+	RETURNING * INTO res;
+	RETURN NEXT res;
+END
+$$LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION add_playfield_type (nameR VARCHAR (100), descriptionR TEXT)
+RETURNS SETOF playfield_types AS
+$$
+DECLARE
+	res playfield_types%ROWTYPE;
+BEGIN
+	INSERT INTO playfield_types (name, description) VALUES (nameR, descriptionR)
+	RETURNING * INTO res;
+	RETURN NEXT res;
+END
+$$LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION add_image (urlI VARCHAR (100), captionI TEXT, playfield_idI INTEGER)
+RETURNS SETOF images AS
+$$
+DECLARE
+	res images%ROWTYPE;
+BEGIN
+	INSERT INTO images (url, caption, playfield_id) VALUES (urlI, captionI, playfield_idI)
+	RETURNING * INTO res;
+	RETURN NEXT res;
+END
+$$LANGUAGE 'plpgsql';
