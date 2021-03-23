a90a7e9b66b8a8f5c2ae2008ef5361a2c3a4fc9b
Rok Žerdoner <rok.zerdoner@scv.si>
2021-03-23 12:30:46 +0100

    loginForm

--- src/FrontEnd/LoginForm.java
+++ src/FrontEnd/LoginForm.java
@@ -1,4 +1,12 @@
 package FrontEnd;
 
+import javax.swing.*;
+
 public class LoginForm {
+    private JLabel PrijavaLabel;
+    private JTextField EmailTextBox;
+    private JPasswordField GesloPasswordField;
+    private JLabel GesloLabel;
+    private JButton PrijavaButton;
+    private JLabel EmailLabel;
 }
