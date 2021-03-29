7a8dbc6aba929048b2f0671efbbd2f757a51ddf4
Rok Žerdoner <rok.zerdoner@scv.si>
2021-03-29 12:55:53 +0200

    Transissions from one form to another

--- src/FrontEnd/LoginForm.java
+++ src/FrontEnd/LoginForm.java
@@ -1,12 +1,6 @@
 package FrontEnd;
 
-import Database.DatabaseManager;
-import Database.User;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.SQLException;
 
 public class LoginForm {
     private JLabel PrijavaLabel;
@@ -16,34 +10,6 @@ public class LoginForm {
     private JButton PrijavaButton;
     private JLabel EmailLabel;
     public JPanel Prijava;
-    private JButton Registracija;
-
-    public LoginForm() {
-        PrijavaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                DatabaseManager db = new DatabaseManager();
-                try {
-                    User user = db.userLogin(EmailTextBox.getText(), GesloPasswordField.toString());
-                    System.out.println(user.Name);
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-        });
-        Registracija.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                JFrame frame = new JFrame("Register");
-                frame.setContentPane(new Register().panel1);
-                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                frame.pack();
-                frame.setVisible(true);
-
-            }
-        });
-    }
 
     public static void main(String[] args){
 
