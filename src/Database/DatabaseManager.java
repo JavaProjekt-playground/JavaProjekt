2274370c219914c0da7a1ffd8eb7e6b9459438bb
Potipecan <podpecan.andraz28@gmail.com>
2021-03-29 12:44:16 +0200

    Tweaks to database manager to make management of server credentials easier.

--- src/Database/DatabaseManager.java
+++ src/Database/DatabaseManager.java
@@ -9,14 +9,20 @@ import java.util.Properties;
 
 public class DatabaseManager {
 
+    final String _server = "ec2-54-228-9-90.eu-west-1.compute.amazonaws.com";
+    final String _port = "5432";
+    final String _database = "db3opa9o4csml6";
+    final String _user = "dczjntmaijrisv";
+    final String _password = "04d47a49ea1ae6e98887d7df0a74bd67d21a410c979ebd4e45570e018eb1d6b3";
+
     private Connection conn;
 
 
     public DatabaseManager(){
-        String url = "jdbc:postgresql://ec2-54-228-9-90.eu-west-1.compute.amazonaws.com:5432/db3opa9o4csml6";
+        String url = String.format("jdbc:postgresql://%s:%s/%s", _server, _port, _database);
         Properties props = new Properties();
-        props.setProperty("user", "dczjntmaijrisv");
-        props.setProperty("password", "04d47a49ea1ae6e98887d7df0a74bd67d21a410c979ebd4e45570e018eb1d6b3");
+        props.setProperty("user", _user);
+        props.setProperty("password", _password);
         props.setProperty("ssl", "false");
 
         try {
@@ -61,8 +67,8 @@ public class DatabaseManager {
      * Adds user to database and updates user object.
      * @param user User to add to database
      * @param password User's password
-     * @return boolean True on success, False on faliure
-     * @throws SQLException
+     * @return boolean True on success, False on failure
+     * @throws SQLException thrown when SQL transaction fails.
      */
     public boolean addUser(User user, String password) throws SQLException {
         return user.selfInsert(conn, password);
@@ -71,10 +77,10 @@ public class DatabaseManager {
     /**
      * Updates user's database entry and refreshes user object.
      * @param user User to update.
-     * @param passChk User's current password for authentication purposess
-     * @param newPass User's new password (pass "" if it stays the same)
-     * @return boolean True on success, False on faliure
-     * @throws SQLException
+     * @param passChk User's current password for authentication purposses
+     * @param newPass User's new password (put "" if it stays the same)
+     * @return boolean True on success, False on failure
+     * @throws SQLException thrown when SQL transaction fails.
      */
     public boolean updateUser(User user, String passChk, String newPass) throws SQLException {
         return user.selfUpdate(conn, passChk, newPass);
