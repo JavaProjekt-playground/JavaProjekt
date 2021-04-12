1ce3c9302e6fc040b6f645ddab36d8c84328ca99
Potipecan <podpecan.andraz28@gmail.com>
2021-04-12 12:45:08 +0200

    Login form password retrieval

--- src/JavaProject.java
+++ src/JavaProject.java
@@ -4,9 +4,10 @@ import FrontEnd.LoginForm;
 import javax.swing.*;
 
 public class JavaProject {
+    public static DatabaseManager db;
 
     public static void main(String[] args){
-        DatabaseManager db = new DatabaseManager();
+        db = new DatabaseManager();
         //JFrame frame = new JFrame("LoginForm");
         JFrame frame = new JFrame("LoginForm");
         frame.setContentPane(new LoginForm().Prijava);
