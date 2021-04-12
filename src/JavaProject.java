b1f7a365dade7a43affd5f91ad9b0233e79ead27
Rok Žerdoner <rok.zerdoner@scv.si>
2021-04-12 12:29:43 +0200

    open add_playground

--- src/JavaProject.java
+++ src/JavaProject.java
@@ -1,5 +1,5 @@
 import Database.DatabaseManager;
-import FrontEnd.LoginForm;
+import FrontEnd.AddPlayground;
 
 import javax.swing.*;
 
@@ -7,11 +7,17 @@ public class JavaProject {
 
     public static void main(String[] args){
         DatabaseManager db = new DatabaseManager();
-        //JFrame frame = new JFrame("LoginForm");
-        JFrame frame = new JFrame("LoginForm");
-        frame.setContentPane(new LoginForm().Prijava);
+        JFrame frame = new JFrame("AddPlayground");
+        frame.setContentPane(new AddPlayground().addplayground);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+        //JFrame frame = new JFrame("LoginForm");
+        /*JFrame frame = new JFrame("LoginForm");
+        frame.setContentPane(new LoginForm().Prijava);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);*/
     }
 }
