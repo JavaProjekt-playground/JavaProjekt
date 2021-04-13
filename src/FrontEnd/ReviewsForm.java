b914f40da2207a8890619a8c3ec9f2c8e1186bee
JureKotnik <jure2002k@gmail.com>
2021-04-13 12:01:47 +0200

    Reviews

--- src/FrontEnd/ReviewsForm.java
+++ src/FrontEnd/ReviewsForm.java
@@ -1,7 +1,6 @@
 package FrontEnd;
 
 import Database.Review;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,8 +16,13 @@ public class ReviewsForm {
         sendReviewButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                JFrame frame = new JFrame("LoginForm");
+                frame.setContentPane(new ReviewsForm().ReviewPanel);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
             }
+
         });
     }
     public static void main(String[] args) {
