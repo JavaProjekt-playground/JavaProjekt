f928107aa85ae7be3d1a3a75f67772df72d16b35
JureKotnik <jure2002k@gmail.com>
2021-04-20 12:43:10 +0200

    Review

--- src/Database/Review.java
+++ src/Database/Review.java
@@ -11,12 +11,16 @@ public class Review implements IUpdatableTable, IInsertableTable {
     public int getID(){return ID;}
     private int userID;
     public int getUserID() {return userID;}
+    private  int playfieldID;
+    public int getPlayfieldID() {return playfieldID;}
     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
     public Review(String message, double score, Timestamp created_at, Timestamp updated_at){
         ID = 0;
         Message = message;
         Score = score;
+        playfieldID = getPlayfieldID();
+        userID = getUserID();
         timestamp = created_at;
         timestamp = updated_at;
     }
@@ -27,8 +31,8 @@ public class Review implements IUpdatableTable, IInsertableTable {
 
     @Override
     public boolean selfInsert(Connection conn, Object... extra) throws SQLException {
-        String command = String.format("SELECT * FROM add_review('%s', '%s', '%s', '%s');",
-                Message, Score, timestamp, timestamp
+        String command = String.format("SELECT * FROM add_review('%s', '%s', '%s', '%s', '%s', '%s');",
+                Message, Score, playfieldID, userID,timestamp.toString(),timestamp.toString()
         );
 
         Statement st = conn.createStatement();
@@ -47,8 +51,8 @@ public class Review implements IUpdatableTable, IInsertableTable {
 
         if (ID < 1) return false;
 
-        String command = String.format("SELECT * FROM update_review( %d, '%s', '%s', '%s', '%s');",
-                ID, Message, Score, timestamp, timestamp
+        String command = String.format("SELECT * FROM update_review( %d, '%s', '%s', '%s', '%s', '%s', '%s');",
+                ID, Message, Score, playfieldID, userID,timestamp.toString(),timestamp.toString()
         );
 
         Statement st = conn.createStatement();
@@ -63,7 +67,6 @@ public class Review implements IUpdatableTable, IInsertableTable {
     }
 
     private void getDataFromResultSet(ResultSet rs) throws SQLException {
-        ID = rs.getInt("id");
         Message = rs.getString("textArea1");
         Score = rs.getDouble("Rating");
     }
--- src/FrontEnd/ReviewsForm.java
+++ src/FrontEnd/ReviewsForm.java
@@ -16,9 +16,9 @@ public class ReviewsForm {
         sendReviewButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame frame = new JFrame("LoginForm");
+                JFrame frame = new JFrame("Main.form");
                 frame.setContentPane(new ReviewsForm().ReviewPanel);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 frame.pack();
                 frame.setVisible(true);
             }
