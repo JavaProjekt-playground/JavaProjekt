d77e84772c992da5eaf35cdb076bc654198b0ed0
Rok Žerdoner <rok.zerdoner@scv.si>
2021-04-27 14:19:15 +0200

    podrobno

--- src/FrontEnd/AddPlayground.java
+++ src/FrontEnd/AddPlayground.java
@@ -1,45 +1,8 @@
 package FrontEnd;
 
-import Database.DatabaseManager;
-import Database.Playfield;
-import Database.Playfield_type;
-import Database.Regions;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.SQLException;
-import java.util.Vector;
-
 
 public class AddPlayground {
-
-    public AddPlayground() throws SQLException {
-        DatabaseManager db = new DatabaseManager();
-        Vector<Regions> region = db.getRegions();
-        for (Regions regions: region) {
-            RegionComboBox.addItem(regions.Name.toString());
-        }
-
-        Vector<Playfield_type> playfield_type = db.getPlayfield_types();
-        for (Playfield_type playfield_types: playfield_type
-             ) {
-            TypeComboBox.addItem(playfield_types.Name);
-        }
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Playfield playfield = new Playfield(AddNameTextField.getText(), DescriptionTextArea.getText(),
-                        AddressTextBox.getText(), EmailTextField.getText(), PhoneTextField.getText(), "", 17, 12, 1, 12);
-                try {
-                    db.addPlayfieldTest(playfield);
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-        });
-    }
-
     private JLabel AddLabel;
     private JLabel NameLabel;
     private JTextField AddNameTextField;
@@ -57,5 +20,5 @@ public class AddPlayground {
     private JLabel PricePerHourLabel;
     private JTextField PricePerHourTextField;
     public JPanel addplayground;
-    private JButton button1;
+    private JButton AddPlaygroundButton;
 }
