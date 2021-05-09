8f2c5a58d922783e24ff04390440ab25deede1cb
Rok Žerdoner <rok.zerdoner@scv.si>
2021-05-09 17:32:44 +0200

    reviews

--- src/FrontEnd/ReviewsForm.java
+++ src/FrontEnd/ReviewsForm.java
@@ -1,6 +1,7 @@
 package FrontEnd;
 
-import Database.*;
+import Database.Playfield;
+import Database.Review;
 
 import javax.swing.*;
 import java.sql.SQLException;
@@ -8,11 +9,13 @@ import java.sql.Timestamp;
 
 public class ReviewsForm implements IFormWindow{
     private JComboBox Rating;
-    private JTextArea textArea1;
+    private JTextArea Text;
     private JButton sendReviewButton;
     private JLabel Message;
     public JPanel mainPanel;
+    private JButton Back;
 
+    private Review review;
     public String title = "Reviews";
 
     @Override
@@ -26,24 +29,43 @@ public class ReviewsForm implements IFormWindow{
     }
 
     public ReviewsForm(Playfield playfield) {
-
+        addObjects(review);
+        getReview();
+        sendReviewButton.addActionListener(e -> Insert(playfield));
     }
 
+
+
     private void Insert(Playfield playfield){
-        DatabaseManager db = new DatabaseManager();
-        User user = App.getCurrentUser();
-        Review review = new Review(
+            Review review = new Review(
                 Message.getText(),
                 Double.valueOf(Rating.getSelectedItem().toString()),
                 playfield.getID(),
-                playfield.UserID,
+                App.getCurrentUser().getID(),
                 new Timestamp(System.currentTimeMillis()),
                 new Timestamp(System.currentTimeMillis())
         );
         try {
-            db.addReview(review);
+            App.DB.addReview(review);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+    }
+    private void addObjects(Review review) {
+        try {
+            Review r = App.DB.getReview(review.getID());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        //Rating.setSelectedItem(review.Score);
+        Text.setText(review.Message);
+    }
+
+    public Review getReview() {
+        return review;
+    }
+    public void setReview(Review review) {
+        this.review = review;
+        title = review == null ? "Add Review" : "Edit Review: " + review.Message;
     }
 }
--- src/FrontEnd/SeeReviews.java
+++ src/FrontEnd/SeeReviews.java
@@ -1,21 +1,43 @@
 package FrontEnd;
 
+import Database.Playfield;
+
 import javax.swing.*;
 
 public class SeeReviews implements IFormWindow{
-    private JTable table1;
-    public JPanel mainPanel;
+    private JTable reviews;
+    public JPanel seeReview;
     private JLabel Reviews;
+    private JButton AddButton;
+    private JButton EditButton;
+    private JButton DeleteButton;
+    private JButton YourReviewsButton;
 
     public String title = "Reviews";
+    private Playfield playfield;
+    public SeeReviews() {
+        AddButton.addActionListener(e -> App.goTo(new ReviewsForm(playfield)));
+        EditButton.addActionListener(e -> App.goTo(new ReviewsForm(playfield)));
+        DeleteButton.addActionListener(e -> Delete());
+    }
 
     @Override
     public JPanel getMainPanel() {
-        return mainPanel;
+        return seeReview;
     }
 
     @Override
     public String getTitle() {
         return "";
     }
+
+    private void Delete(){
+
+    }
+
+    /*private Review getSelectedReview(){
+        int i = reviews.getSelectedRow();
+        Review r = i > -1 ? ((ReviewsTableModel)reviews.getModel()).getReview(i) : null;
+        return r;
+    }*/
 }
