2d973dd5397f0c7457e06dcbb810551b7296f724
Rok Žerdoner <rok.zerdoner@scv.si>
2021-05-11 11:20:52 +0200

    ReservationsForm output date

--- src/FrontEnd/ReservationsForm.java
+++ src/FrontEnd/ReservationsForm.java
@@ -4,22 +4,25 @@ import Database.Playfield;
 import Database.Reservation;
 
 import javax.swing.*;
-import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 public class ReservationsForm implements IFormWindow {
     private JLabel NameT;
-    private JTextField SurnameT;
-    private JTextField EmailT;
+    private JLabel SurnameT;
+    private JLabel EmailT;
     private JSpinner DateFromT;
-    private JTextField DateToT;
+    private JSpinner DateToT;
     private JLabel AddL;
     private JTable AddTable;
     private JButton AddButton;
     private JButton BackButton;
     private JPanel mainPanel;
+    private JLabel Title;
 
     private boolean mode;
     private String title;
+    private Playfield play = null;
 
     @Override
     public JPanel getMainPanel() {
@@ -36,16 +39,21 @@ public class ReservationsForm implements IFormWindow {
         this(playfield, null);
     }
     public ReservationsForm(Playfield playfield, Reservation reservation){
-        mode = Mode(playfield, reservation);
+        play = playfield;
+        mode = Mode(reservation);
         addObject(playfield, reservation);
         DateFromT.setModel(new SpinnerDateModel());
+        DateToT.setModel(new SpinnerDateModel());
         BackButton.addActionListener(e -> Back());
+        AddButton.addActionListener(e -> Insert(play, reservation));
     }
 
-    public boolean Mode(Playfield playfield, Reservation reservation) {
+    public boolean Mode(Reservation reservation) {
         if (reservation != null) {
+            Title.setText("Spreminjanje podatkov naročila");
             return true;
         } else {
+            Title.setText("Naročanje");
             return false;
         }
     }
@@ -55,24 +63,31 @@ public class ReservationsForm implements IFormWindow {
         SurnameT.setText(App.getCurrentUser().Surname);
         EmailT.setText(App.getCurrentUser().Email);
         if(mode){
+            //SET VALUE TO DATE SPINNER
             //DateFromT.setText(String.valueOf(reservation.FromDate));
-            DateToT.setText(String.valueOf(reservation.ToDate));
+            //DateToT.setText(String.valueOf(reservation.ToDate));
         }
     }
 
-
+    private void Insert(Playfield playfield, Reservation reservation){
+        if(mode){}
+        else {
+                Reservation res = new Reservation(
+                    UtilsH.convertStringToTimestamp(new SimpleDateFormat("yyyy-MM-dd").format(DateFromT.getValue())),
+                    UtilsH.convertStringToTimestamp(new SimpleDateFormat("yyyy-MM-dd").format(DateToT.getValue())),
+                    new Timestamp(System.currentTimeMillis()),
+                    false,
+                    1,
+                    App.getCurrentUser().getID(),
+                    playfield.getID()
+                );
+                res.toString();
+        }
+    }
 
     private void Back(){
         App.canGoBack();
         App.goBack();
-    }
-
-    public static void main(String[] args){
-        try {
-            App.goTo(new ReservationsForm(App.DB.getPlayfield(31)));
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
     }
 
 
