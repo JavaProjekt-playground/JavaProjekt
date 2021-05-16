1008fed93347bbdb541c3da2fe3a60bbf5b68382
Potipecan <podpecan.andraz28@gmail.com>
2021-05-16 19:43:14 +0200

    removed separate class library for database

--- Project2MobileApp/Project2MobileApp/App.xaml.cs
+++ Project2MobileApp/Project2MobileApp/App.xaml.cs
@@ -3,15 +3,19 @@ using Project2MobileApp.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Project2MobileApp.Database;
 
 namespace Project2MobileApp
 {
     public partial class App : Application
     {
+        public static DatabaseManager DB;
 
         public App()
         {
             InitializeComponent();
+
+            DB = new DatabaseManager();
 
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
---
+++ Project2MobileApp/Project2MobileApp/Database/DatabaseManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Npgsql;
+
+namespace Project2MobileApp.Database
+{
+    public class DatabaseManager
+    {
+        private const string _server = "ec2-54-228-9-90.eu-west-1.compute.amazonaws.com";
+        private const string _port = "5432";
+        private const string _database = "db3opa9o4csml6";
+        private const string _user = "dczjntmaijrisv";
+        private const string _password = "04d47a49ea1ae6e98887d7df0a74bd67d21a410c979ebd4e45570e018eb1d6b3";
+
+        private NpgsqlConnection _conn;
+
+        public DatabaseManager()
+        {
+            string connString = $"Host={_server}:{_port};Username={_user};Password={_password};Database={_database}";
+            _conn = new NpgsqlConnection(connString);
+        }
+
+    }
+}
